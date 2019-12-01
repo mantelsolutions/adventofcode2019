39ff4e149e6309ec3c2b973e30cff605a9515ae5
Matthias Mantel <matthias.mantel@gmx.net>
2019-12-01 10:36:36 +0100

    Day1 solution

---
+++ day1/day1.py
@@ -0,0 +1,30 @@
+import math
+
+lines = [line.rstrip('\n') for line in open('input.txt')]
+
+# part1
+fuel = 0
+for line in lines:
+    fuelForModule = math.floor(int(line) / 3) - 2
+    fuel += fuelForModule
+
+print("Fuel needed: " + str(fuel))
+
+# part2
+def calculateFuelForMass(mass):
+    """calculate the fuel for a given mass"""
+    return math.floor(int(mass) / 3) - 2
+
+def recursiveCalc(mass):
+    """Recursive calculation of fuel required"""    
+    if(mass > 0):
+       return mass + recursiveCalc(calculateFuelForMass(mass))
+    else:
+        return 0
+
+fuel = 0
+for line in lines:
+    fuel += recursiveCalc(calculateFuelForMass(int(line)))
+
+print("Part2 - fuel needed: " + str(fuel))
+
