0ed393741d8bfd7227f2874ad85680a26c312e9d
Matthias Mantel <matthias.mantel@gmx.net>
2019-12-02 20:13:20 +0100

    solution for part2 of day2

--- day2/day2.py
+++ day2/day2.py
@@ -4,50 +4,83 @@ origValues = list(map(int, open('day2/input.txt').readline().rstrip().split(',')
 
 print(origValues)
 
+def getOpCode(values, codeAndArgumentWidth, numberOfCodesEvaluated):
+    currentOpCodeIndex = getCurrentOpCodeIndex(values, codeAndArgumentWidth, numberOfCodesEvaluated)
+    return int(values[currentOpCodeIndex])
+
+def addOperation(values, opCodeIndex):
+    operant1 = int(values[values[opCodeIndex+1]])
+    operant2 = int(values[values[opCodeIndex+2]])
+    values[values[opCodeIndex+3]] = operant1 + operant2
+
+def multiplyOperation(values, opCodeIndex):
+    operant1 = int(values[valuesPart1[opCodeIndex+1]])
+    operant2 = int(values[valuesPart1[opCodeIndex+2]])
+    values[values[opCodeIndex+3]] = operant1 * operant2
+
+def getCurrentOpCodeIndex(values, codeAndArgumentWidth, numberOfCodesEvaluated):
+    return codeAndArgumentWidth * numberOfCodesEvaluated
+
+
+def getResult(values):
+    numberOfCodesEvaluated = 0
+    codeAndArgumentWidth = 4
+    haltCodeFound = False
+    while  not haltCodeFound:
+        opCode = getOpCode(values, codeAndArgumentWidth, numberOfCodesEvaluated)
+        #print("Current opCode: " + str(opCode))
+
+        if(opCode == 1):
+            #print("add operation")
+            addOperation(values, getCurrentOpCodeIndex(values, codeAndArgumentWidth, numberOfCodesEvaluated))
+        elif(opCode == 2):
+            #print("multiply operation")
+            multiplyOperation(values, getCurrentOpCodeIndex(values, codeAndArgumentWidth,numberOfCodesEvaluated))
+        elif(opCode == 99):
+            #print("HALT found")
+            haltCodeFound = True
+        else:
+            print("ERROR unknown opcode found: " + str(opCode) + " for opCodeIndex: " + str(getCurrentOpCodeIndex(values, codeAndArgumentWidth, numberOfCodesEvaluated)))
+            return -1
+
+        numberOfCodesEvaluated += 1
+    return values[0]
+
 valuesPart1 = origValues.copy()
 
 valuesPart1[1] = 12
 valuesPart1[2] = 2
 
-opCodeIndex = 0
-numberOfCodesEvaluated = 0
-codeAndArgumentWidth = 4
+resultPart1 = getResult(valuesPart1)
+print("Result of part1: " + str(valuesPart1[0]))
+assert 5098658 == resultPart1
 
-def getOpCode(codeAndArgumentWidth, numberOfCodesEvaluated):
-    currentOpCodeIndex = getCurrentOpCodeIndex(codeAndArgumentWidth, numberOfCodesEvaluated)
-    return int(valuesPart1[currentOpCodeIndex])
+# part2
+valuesPart2 = origValues.copy()
 
-def addOperation(opCodeIndex):
-    operant1 = int(valuesPart1[valuesPart1[opCodeIndex+1]])
-    operant2 = int(valuesPart1[valuesPart1[opCodeIndex+2]])
-    valuesPart1[valuesPart1[opCodeIndex+3]] = operant1 + operant2
+noun = 0
+verb = 0
+desiredOutput = 19690720
 
-def multiplyOperation(opCodeIndex):
-    operant1 = int(valuesPart1[valuesPart1[opCodeIndex+1]])
-    operant2 = int(valuesPart1[valuesPart1[opCodeIndex+2]])
-    valuesPart1[valuesPart1[opCodeIndex+3]] = operant1 * operant2
+desiredOutputFound = False
+while not desiredOutputFound:
+    # print("Noun: " + str(noun) + " -- Verb: " + str(verb))
+    valuesPart2[1] = noun
+    valuesPart2[2] = verb
 
-def getCurrentOpCodeIndex(codeAndArgumentWidth, numberOfCodesEvaluated):
-    return codeAndArgumentWidth * numberOfCodesEvaluated
-
-haltCodeFound = False
-while  not haltCodeFound:
-    opCode = getOpCode(codeAndArgumentWidth, numberOfCodesEvaluated)
-    print("Current opCode: " + str(opCode))
-
-    if(opCode == 1):
-        print("add operation")
-        addOperation(getCurrentOpCodeIndex(codeAndArgumentWidth, numberOfCodesEvaluated))
-    elif(opCode == 2):
-        print("multiply operation")
-        multiplyOperation(getCurrentOpCodeIndex(codeAndArgumentWidth,numberOfCodesEvaluated))
-    elif(opCode == 99):
-        print("HALT found")
-        haltCodeFound = True
-    else:
-        print("ERROR unknown opcode found: " + str(opCode))
+    currentResult = getResult(valuesPart2)
+    if(currentResult < 0):
+        print("Found unknown opcode ==> FAIL")
         break
 
-    numberOfCodesEvaluated += 1
+    if currentResult == desiredOutput:
+        desiredOutputFound = True
+    else:
+        if verb >= 99:
+            noun += 1
+            verb = 0
+        else:
+            verb += 1  
+        valuesPart2 = origValues.copy()
 
-print("Result of part1: " + str(valuesPart1[0]))
+print("Result part2: " + str(100 * valuesPart2[1] + valuesPart2[2]))
