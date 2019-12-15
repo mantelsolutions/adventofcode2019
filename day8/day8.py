bdffdfc6e845f4cd82ca6ade4d9c31e1afeeed9c
Matthias Mantel <matthias.mantel@gmx.net>
2019-12-15 14:13:45 +0100

    day8 part2

--- day8/day8.py
+++ day8/day8.py
@@ -1,9 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plot
+
 lines = [line.rstrip('\n') for line in open('day8/input.txt')]
 
 pixels = lines[0]
 
 width = 25
 height = 6
+
 layers = list()
 
 digitsPerLayer = width * height
@@ -27,9 +31,13 @@ class Layer:
     def getAmountOfDigit(self, digit):
         return self.digitToAmountMap.get(digit, -1)
 
+    def __repr__(self):
+        return "Digits in layer: {}".format(self.digits)
+
 currentLayer = Layer()
 
 for digit in pixels:
+    
     if len(currentLayer.digits) < digitsPerLayer:
         currentLayer.addDigit(int(digit))
     else:
@@ -37,8 +45,31 @@ for digit in pixels:
         currentLayer = Layer()
         currentLayer.addDigit(int(digit))
 
+layers.append(currentLayer)
+
+resultArray = [[]]
+resultArray = np.full((width,height),3)
+
+for position in range(0,width*height):
+    pixel = 2
+    for layer in layers:
+        assert(len(layer.digits) == (width * height))
+        currentDigit = layer.digits[position]
+        if currentDigit < 2:
+            pixel = currentDigit
+            break
+    x = position % width
+    y = position // width
+    resultArray[x][y] = pixel
+
+print(resultArray)
+
+plot.matshow(resultArray)
+plot.show()        
+    
+
 layers.sort(key=lambda x: x.numberOfZeros)
 
 resultPart1 = layers[0].getAmountOfDigit(1) * layers[0].getAmountOfDigit(2)
 
-print("Result part1 {}".format(resultPart1))
+print("Result part1: {}".format(resultPart1))
